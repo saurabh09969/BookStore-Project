1d6c5b1ed1d75be80a9de23fb0e2505c8c0f1b26
saurabh09969 <79187029+saurabh09969@users.noreply.github.com>
2024-08-07 00:30:07 +0530

    BookStore

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://saurabhrautela066:Mfgenius%401234%23@book-store.vea9qgn.mongodb.net/?retryWrites=true&w=majority&appName=book-store'
+//adding special character to password gives error 
+//use URL-encoded password to resolve this issue
---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import  express  from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import {book} from './models/bookModel.js'
+
+const app = express();
+
+app.get('/',(request,response) => {
+    console.log(request);
+    return response.status(234).send("MERN Stack")
+});
+
+
+//Route to save a New Book
+
+
+
+
+mongoose.connect(mongoDBURL)
+.then(() => {
+    console.log("App successfully connceted to database");
+    
+    app.listen(PORT, () => {
+        console.log(`App is listening on port:${PORT}`);
+    });
+        
+})
+
+.catch((error) => {
+    console.log(error);
+})
