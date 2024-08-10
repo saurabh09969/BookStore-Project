cc4f8ad73732c5818d0c52601dafc926aa87575a
saurabh09969 <79187029+saurabh09969@users.noreply.github.com>
2024-08-11 00:49:48 +0530

    added some frontend and changes in backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import  express  from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import booksRoutes from './routes/booksRoutes.js';
+import cors from 'cors';
 
 
 const app = express();
@@ -9,6 +10,17 @@ const app = express();
 //Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 
 app.get('/',(request,response) => {
